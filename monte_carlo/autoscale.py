b053f80e1250983b5a83415acd79ebfd0c95b6c2
Jan <jan.tecklenburg@gmx.de>
2024-05-31 14:25:24 +0200

    Estimating the static storage efficiency using Monte Carlo

---
+++ monte_carlo/autoscale.py
@@ -0,0 +1,10 @@
+from contextlib import contextmanager
+import matplotlib.pyplot as plt
+
+@contextmanager
+def autoscale_turned_off(ax=None):
+  ax = ax or plt.gca()
+  lims = [ax.get_xlim(), ax.get_ylim()]
+  yield
+  ax.set_xlim(*lims[0])
+  ax.set_ylim(*lims[1])
